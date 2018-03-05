cee08eca43240b86ff14c640ef7c59f125828fdc
Pavitra Shadvani <pavitrashadvani122@gmail.com>
2018-03-05 12:35:52 -0600

    Update GuardianScraper.py

--- TheGuardian/GuardianScraper.py
+++ TheGuardian/GuardianScraper.py
@@ -8,18 +8,12 @@ class theGuardian :
 		self.headlineList=[]
 		r=requests.get("https://www.theguardian.com/us")
 		self.soup=BeautifulSoup(r.content,'html.parser')
-		#print(self.soup.prettify())
-		#self.headlines=(self.soup.find_all(id="headlines"))
-		#self.soup=BeautifulSoup((self.soup.find("#headlines")),'html.parser')
 		self.soup=self.soup.find('section',id="headlines")
 		self.headlines=self.soup.find_all(class_="fc-item__title")
-		#print self.headlines
 		for headline,j in zip(self.headlines,range(noOfHeadlines)):
 			self.headlineList.append("")
-			#print j,len(self.headlineList )
 			self.headlineList[j]=""
 			a=headline.find_all(class_="js-headline-text")
-			#self.headlineList[i]
 			try:
 				for i in a:
 					print (i.string)
@@ -29,7 +23,6 @@ class theGuardian :
 				j=j-1;
 				self.headlineList.pop()
 				continue
-			#print self.headlineList
 			print ("\n")
 
 		print self.headlineList
