794ff7e8b77ce8cb6d94e3932612ec6e84f8542c
AGeubelleGit <amjg2102@gmail.com>
2018-03-08 19:49:01 -0600

    Get headlines from reddit/r/news

---
+++ Reddit/newsHeadlines.py
@@ -0,0 +1,28 @@
+import bs4
+import requests
+import json
+import datetime
+import urllib.parse
+
+from NewsArticle import NewsArticle
+
+
+def main():
+    headlines = get_news_front_page_headlines()
+
+
+def get_news_front_page_headlines(num_headlines=None):
+    headlines = list()
+    url = "https://www.reddit.com/r/news/hot.json"
+    json_str = requests.get(url, headers = {'User-agent': 'your bot 0.2'}).text
+    data = json.loads(json_str)
+    for i in data["data"]["children"]:
+        the_data = i["data"]
+        if "title" in the_data.keys():
+            headlines.append(NewsArticle(the_data["title"]))
+
+    return headlines
+
+
+if __name__ == '__main__':
+    main()
