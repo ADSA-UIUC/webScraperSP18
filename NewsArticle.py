08ea90a7d4077730ce889d3e4bd475a0ed72b57d
Reetahan Mukhopadhyay <reetahan@gmail.com>
2018-04-08 17:05:46 -0500

    Update NewsArticle.py
    updatee

--- NewsArticle.py
+++ NewsArticle.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-import pickle
-import json
-# from BBC_Parser import Search as bbc
-# from Cnet_Parser import Parser as cnet
-# from Reddit import newsHeadlines as red
-# from HackerNews import Search as hack
-=======
 
->>>>>>> refs/remotes/origin/reeeeeet
 
 
 class NewsArticle:
@@ -23,38 +14,3 @@ class NewsArticle:
 		self.text = aText #string - The text body of the article
 		self.image_desc = aImage_desc #string - A list of image descriptions - one entry per image
 
-<<<<<<< HEAD
-	#articles - list of article objects
-	#sourceName - text string of the articles source i.e. [article].source
-	@staticmethod
-	def dumpArticles(articles, sourceName):
-		with open("Headlines\{}Articles.dat".format(sourceName), "wb") as f:
-			pickle.dump(articles, f)
-
-	@staticmethod
-	def loadArticles(sourceName):
-		with open("Headlines/{}Articles.dat".format(sourceName), "rb") as f:
-			return pickle.load(f)
-
-	@staticmethod
-	def dumpAllArticles():
-		BBCarts = bbc.retrieve_homepage_articles()
-		CNETarts = cnet.get_headlines()
-		HACKarts = hack.retrieve_homepage_articles()
-		REDarts = red.get_news_front_page_headlines()
-
-		allArts = [BBCarts, CNETarts, HACKarts, REDarts]
-		for art in allArts:
-			dumpArticles(art, art[0].source)
-
-#dumpAllArticles()
-if __name__=="__main__":
-    A = NewsArticle.loadArticles("cnn")
-    print([a.title for a in A[:5]])
-    A = NewsArticle.loadArticles("reddit")
-    print([a.title for a in A[:5]])
-    A = NewsArticle.loadArticles("cnet")
-    print([a.title for a in A[:5]])
-=======
-	#
->>>>>>> refs/remotes/origin/reeeeeet
