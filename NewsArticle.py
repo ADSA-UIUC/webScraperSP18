b51e430046e546a19647bd6cb2dc8f11cbad287d
Reetahan Mukhopadhyay <reetahan@gmail.com>
2018-04-08 16:42:47 -0500

    baggage is bad boiz

--- NewsArticle.py
+++ NewsArticle.py
@@ -1,8 +1,4 @@
-import pickle
-from BBC_Parser import Search as bbc
-from Cnet_Parser import Parser as cnet
-from Reddit import newsHeadlines as red
-from HackerNews import Search as hack
+
 
 
 class NewsArticle:
@@ -18,27 +14,4 @@ class NewsArticle:
 		self.text = aText #string - The text body of the article
 		self.image_desc = aImage_desc #string - A list of image descriptions - one entry per image
 
-	#articles - list of article objects
-	#sourceName - text string of the articles source i.e. [article].source
-	@staticmethod
-	def dumpArticles(articles, sourceName):
-		with open("Headlines\{}Articles.dat".format(sourceName), "wb") as f:
-			pickle.dump(articles, f)
-
-	@staticmethod
-	def loadArticles(sourceName):
-		with open("Headlines\{}Articles.dat".format(sourceName), "rb") as f:
-			return pickle.load(f)
-
-	@staticmethod
-	def dumpAllArticles():
-		BBCarts = bbc.retrieve_homepage_articles()
-		CNETarts = cnet.get_headlines()
-		HACKarts = hack.retrieve_homepage_articles()
-		REDarts = red.get_news_front_page_headlines()
-
-		allArts = [BBCarts, CNETarts, HACKarts, REDarts]
-		for art in allArts:
-			dumpArticles(art, art[0].source)
-
-dumpAllArticles()
+	#
