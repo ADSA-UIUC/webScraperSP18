f7e49a9af39a5c8513e12d112a64006daa73ee94
jsteele13 <jenni1317@gmail.com>
2018-04-15 16:08:39 -0500

    underscore not camelcase

--- NewsSource.py
+++ NewsSource.py
@@ -3,10 +3,10 @@ from interface import Interface
 class NewsSource(Interface):
 
 	# return NewsArticle objects for articles on homepage
-    def getHomepageArticles(self):
+    def get_homepage_articles(self):
         pass
 
    	# return NewsArticle objects for results of a keyword search 
-    def getKeywordSearchArticles(self, keyword):
+    def get_keyword_search_articles(self, keyword):
     	pass
     
