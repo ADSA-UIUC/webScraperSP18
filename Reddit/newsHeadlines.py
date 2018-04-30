25131c7dcce77cffe885f31669a5b258a01e8c3a
AGeubelleGit <amjg2102@gmail.com>
2018-04-29 21:33:20 -0500

    hi:

--- Reddit/newsHeadlines.py
+++ Reddit/newsHeadlines.py
@@ -3,33 +3,49 @@ import requests
 import json
 import datetime
 import urllib.parse
-
-#from NewsArticle import NewsArticle
-
-
-def main():
-    headlines = get_news_front_page_headlines()
-    print(headlines)
-    #NewsArticle.dumpArticles(headlines, "reddit")
-    #headlines = NewsArticle.loadArticles("reddit")
-    # for headline in headlines:
-    #     print(headline)
+from NewsArticle import NewsArticle
 
 
-def get_news_front_page_headlines(num_headlines=None):
+# class reddit_scraper:
+#     def __init__(self):
+#         pass
+
+
+# return NewsArticle objects for articles on homepage
+# BAILEY CALL THIS FUNCTION
+def get_homepage_articles(self):
     headlines = list()
-    #url = "https://www.reddit.com/r/news/hot.json"
-    url = "https://www.reddit.com/r/uiuc/hot.json"
+    subreddit = "all"
+    url = "https://www.reddit.com/r/" + subreddit + "/hot.json"
     json_str = requests.get(url, headers = {'User-agent': 'your bot 0.2'}).text
     data = json.loads(json_str)
     for i in data["data"]["children"]:
         the_data = i["data"]
         if "title" in the_data.keys():
-            #headlines.append(NewsArticle(the_data["title"]))
-            headlines.append(the_data["title"])
+            new_article = NewsArticle()
+            new_article.title = the_data["title"]
+            headlines.append(new_article)
 
     return headlines
 
 
-if __name__ == '__main__':
-    main()
+# return NewsArticle objects for results of a keyword search 
+def get_keyword_search_articles(self, keyword):
+    pass
+
+    # def main():
+    #     url = "https://www.reddit.com/r/news/search?q=Trump&restrict_sr=on&sort=relevance&t=month"
+    #     json_str = requests.get(url, headers = {'User-agent': 'your bot 0.2'}).text
+    #     print(json_str)
+
+    #     #headlines = get_news_front_page_headlines()
+    #     #print(headlines)
+    #     #NewsArticle.dumpArticles(headlines, "reddit")
+    #     #headlines = NewsArticle.loadArticles("reddit")
+    #     # for headline in headlines:
+    #     #     print(headline)
+
+
+
+# if __name__ == '__main__':
+#     main()
